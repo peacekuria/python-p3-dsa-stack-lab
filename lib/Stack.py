b33ef35b79b6b93ace96022aa2c2749fbc12b1f8
Ephy <macbookpro@Ephys-MacBookPro.local>
2025-11-29 08:17:34 +0300

    done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,34 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        self.items = list(items)
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            index = self.items.index(target)
+            return len(self.items) - 1 - index
+        return -1
